03eac160f260d6132c68fbfeb8d0dd3d3e75b7b8
20220255 <118017744+20220255@users.noreply.github.com>
2024-01-22 19:11:54 +1300

    Home Page

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -4,7 +4,8 @@ function Home() {
   return (
     <>
       <section className="heading">
-        <h1>Automated Loyalty Program System</h1>
+        <h1>New Year Promo!!!</h1>
+        <p>Wash or dry your laundry for 6 times and get a free wash on your next visit.</p>
 
       </section>
       
