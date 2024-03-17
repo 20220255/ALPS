323f16465206e7c04813fff64ec9b46adeaf5c57
20220255 <118017744+20220255@users.noreply.github.com>
2024-03-17 16:58:35 +1300

    Ref ID is based on Points docs

--- backend/utils/generateRefId.js
+++ backend/utils/generateRefId.js
@@ -1,8 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const Points = require("../models/pointsModel");
 
 const generateRefId = asyncHandler(async () => {
-  const numDocs = await  User.countDocuments();
+  const numDocs = await  Points.countDocuments();
   const generateRandomString = function (length = 6) {
     return Math.random().toString(20).substr(2, length).toLocaleUpperCase();
   };
