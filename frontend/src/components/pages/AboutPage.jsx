d2b8a1ae011300495543f9e72c24bc0a7ee1a5c7
20220255 <118017744+20220255@users.noreply.github.com>
2024-03-29 14:01:09 +1300

    Update video

--- frontend/src/components/pages/AboutPage.jsx
+++ frontend/src/components/pages/AboutPage.jsx
@@ -9,20 +9,19 @@ function AboutPage() {
         <p>
           Lorem ipsum dolor sit amet. Qui mollitia molestiae in corporis sint id
           voluptas consequatur quo nobis repudiandae. Ea quia nihil sed
-          exercitationem distinctio et nesciunt eius sit recusandae natus sed
-          perspiciatis rerum nam exercitationem expedita id explicabo tempora.
+          exercitationem distinctio et nesciunt eius sit recusandae natus.
         </p>
         <iframe
           title="LG Washing Machines"
           src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FAbans.lk%2Fvideos%2F1099174626790978%2F&show_text=false&width=560&t=0"
-          width="560"
-          height="314"
+          width="250"
+          height="165"
           style={{ border: "none", overflow: "hidden" }}
           frameborder="0"
-          allowfullscreen="true"
+          allowfullscreen="false"
           allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
           allowFullScreen="true"
-        ></iframe>
+        />
 
         <p>
           <Link to="/">Back to Home</Link>
