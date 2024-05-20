c88c75717c0863872a9e12c1aad7a6c4d3b32f03
20220255 <118017744+20220255@users.noreply.github.com>
2024-05-20 22:34:09 +1200

    weather app update

--- frontend/src/components/pages/WeatherPage.jsx
+++ frontend/src/components/pages/WeatherPage.jsx
@@ -14,9 +14,13 @@ function WeatherPage() {
   });
 
   const lagroWeather = async (event) => {
+    const latitude = 14.7307951;
+    const longitude = 121.0672858;
+    const apiKey = "33aa6b1da01b6c1ac5af7206a25f8551";
+    const unit = "metric";
     try {
       const resp = await axios.get(
-        "https://api.openweathermap.org/data/2.5/weather?lat=14.7307951&lon=121.0672858&appid=33aa6b1da01b6c1ac5af7206a25f8551&units=metric"
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`
       );
       const weather = await resp.data;
       setWeather(weather);
@@ -62,49 +66,49 @@ function WeatherPage() {
 
   return (
     <Card>
-      {/* <div className="app-weather"> */}
-        <h1 className="app-name">Snapwash Weather App</h1>
-        {weather.loading && (
-          <>
-            <br />
-            <br />
-            <Oval type="Oval" color="black" height={100} width={100} />
-          </>
-        )}
-        {weather.error && (
-          <>
-            <br />
-            <br />
-            <span className="error-message">
-              <FontAwesomeIcon icon={faFrown} />
-              <span style={{ fontSize: "20px" }}>City not found</span>
-            </span>
-          </>
-        )}
-        {weather && weather.main && (
-          <div>
-            <div className="city-name">
-              <h2>Brgy Greater Lagro</h2>
-            </div>
-            <div className="date">
-              <span>{toDateFunction()}</span>
-            </div>
-            <div className="icon-temp">
-              <img
-                className=""
-                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                alt={weather.weather[0].description}
-              />
-              {Math.round(weather.main.feels_like)}
-              <sup className="deg">°C</sup>
-            </div>
-            <div className="des-wind">
-              <p>{weather.weather[0].description.toUpperCase()} - Temperature Feel</p>
-              <p>Wind Speed: {weather.wind.speed}m/s</p>
-            </div>
+      <h1 className="app-name">Snapwash Weather App</h1>
+      {weather.loading && (
+        <>
+          <br />
+          <br />
+          <Oval type="Oval" color="black" height={100} width={100} />
+        </>
+      )}
+      {weather.error && (
+        <>
+          <br />
+          <br />
+          <span className="error-message">
+            <FontAwesomeIcon icon={faFrown} />
+            <span style={{ fontSize: "20px" }}>City not found</span>
+          </span>
+        </>
+      )}
+      {weather && weather.main && (
+        <div>
+          <div className="city-name">
+            <h2>Brgy Greater Lagro</h2>
           </div>
-        )}
-      {/* </div> */}
+          <div className="date">
+            <span>{toDateFunction()}</span>
+          </div>
+          <div className="icon-temp">
+            <img
+              className=""
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            {Math.round(weather.main.feels_like)}
+            <sup className="deg">°C</sup>
+          </div>
+          <div className="des-wind">
+            <p>
+              {weather.weather[0].description.toUpperCase()} - Temperature Feel
+            </p>
+            <p>Wind Speed: {weather.wind.speed}m/s</p>
+          </div>
+        </div>
+      )}
     </Card>
   );
 }
